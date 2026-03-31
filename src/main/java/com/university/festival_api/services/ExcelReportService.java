a18c7a5940365dee54d380b418c9c8db46f45651
Volodymyr1939rq <rudenkovolodymyr086@gmail.com>
2026-03-31 18:22:13 +0300

    Fixed the ExelReportService

--- src/main/java/com/university/festival_api/services/ExcelReportService.java
+++ src/main/java/com/university/festival_api/services/ExcelReportService.java
@@ -3,6 +3,7 @@ package com.university.festival_api.services;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -21,7 +22,7 @@ public class ExcelReportService {
         ByteArrayOutputStream out=new ByteArrayOutputStream()){
         Sheet sheet=workbook.createSheet("Festival Sheet");
         Row headerRow=sheet.createRow(0);
-        String[] colums={"ID", "Ім'я", "Прізвище", "Група", "Жанр", "Тур 1", "Тур 2", "Тур 3", "Загальний бал"};
+        String[] colums={"ID", "Ім'я", "Прізвище", "Група", "Жанр", "Тур 1", "Тур 2", "Тур 3", "Загальний бал","Локація","Реквізит","Техніка","Приз"};
         for(int i=0;i<colums.length;i++){
             Cell cell=headerRow.createCell(i);
             cell.setCellValue(colums[i]);
@@ -40,7 +41,28 @@ public class ExcelReportService {
 
             int totalScore=participant.getTour1Score()+participant.getTour2Score()+participant.getTour3Score();
             row.createCell(8).setCellValue(totalScore);
+
+            String venue=(participant.getVenue()!=null && participant.getVenue().getName()!=null)
+            ? participant.getVenue().getName():"Не вказано";
+            row.createCell(9).setCellValue(venue);
+
+            String propsStr="Немає";
+            if(participant.getProplist()!=null && !participant.getProplist().isEmpty()){
+              propsStr=participant.getProplist().stream().map(prop->prop.getTitle()).collect(Collectors.joining(", "));
+            }
+            row.createCell(10).setCellValue(propsStr);
+
+            String equipStr="Немає";
+            if(participant.getEquipments()!=null && !participant.getEquipments().isEmpty()){
+                equipStr=participant.getEquipments().stream().map(eq->eq.getName()).collect(Collectors.joining(", "));
+            }
+            row.createCell(11).setCellValue(equipStr);
+
+            String prize=(participant.getPrize()!=null && participant.getPrize().getTitle()!=null)
+            ? participant.getPrize().getTitle():"-";
+            row.createCell(12).setCellValue(prize);
         }
+        
         for(int i=0;i<colums.length;i++){
             sheet.autoSizeColumn(i);
         }
